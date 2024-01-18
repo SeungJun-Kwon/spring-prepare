9958776f2b7c5362d010239f0c4398ee6b5a853d
DAchoMI <tyrnsorl123@naver.com>
2024-01-18 20:56:38 +0900

    :white_check_mark: HTTP 데이터를 Params, 객체 등으로 처리하는 방법

---
+++ src/main/java/com/sparta/springprepare/request/Star.java
@@ -0,0 +1,11 @@
+package com.sparta.springprepare.request;
+
+public class Star {
+    public String name;
+    public int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
