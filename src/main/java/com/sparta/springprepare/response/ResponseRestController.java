377ed8c8bf5a2ab579a48503af215e0a17edb139
DAchoMI <tyrnsorl123@naver.com>
2024-01-18 19:39:05 +0900

    :white_check_mark: Data를 Client에 반환하는 방법

--- src/main/java/com/sparta/springprepare/response/ResponseRestController.java
+++ src/main/java/com/sparta/springprepare/response/ResponseRestController.java
@@ -1,20 +1,19 @@
-package com.sparta.springmvc.response;
+package com.sparta.springprepare.response;
 
-import com.sparta.springprepare.response.Star;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/response")
-public class ResponseController {
+// RestController = Controller + ResponseBody
+// 전부 ResponseBody로 반환되기 때문에 View를 반환해야 하는 클래스에서는 사용하면 안 됨.
+@RestController
+@RequestMapping("/response/rest")
+public class ResponseRestController {
     // [Response header]
     //   Content-Type: text/html
     // [Response body]
     //   {"name":"Robbie","age":95}
     @GetMapping("/json/string")
-    @ResponseBody
     public String helloStringJson() {
         return "{\"name\":\"Robbie\",\"age\":95}";
     }
@@ -24,7 +23,6 @@ public class ResponseController {
     // [Response body]
     //   {"name":"Robbie","age":95}
     @GetMapping("/json/class")
-    @ResponseBody
     public Star helloClassJson() {
         return new Star("Robbie", 95);
     }
