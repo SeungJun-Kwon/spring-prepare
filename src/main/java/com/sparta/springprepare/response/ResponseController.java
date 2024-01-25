ec691b96928c84c2d8b03d416ba86b6cb2206f8c
DAchoMI <tyrnsorl123@naver.com>
2024-01-25 10:22:30 +0900

    :white_check_mark: 영속성 Test 코드

--- src/main/java/com/sparta/springprepare/response/ResponseController.java
+++ src/main/java/com/sparta/springprepare/response/ResponseController.java
@@ -1,6 +1,5 @@
-package com.sparta.springmvc.response;
+package com.sparta.springprepare.response;
 
-import com.sparta.springprepare.response.Star;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/sparta/springprepare/jpa/EntityTest.java
+++ src/test/java/com/sparta/springprepare/jpa/EntityTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EntityTest {
-
     EntityManagerFactory emf;
     EntityManager em;
 
