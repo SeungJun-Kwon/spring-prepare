3026f2f5c28df3a7acc20580342017f6c0ccdfcd
DAchoMI <tyrnsorl123@naver.com>
2024-01-26 19:40:43 +0900

    :sparkles: 로그인 구현(비밀번호 matches)

--- src/main/java/com/sparta/springprepare/auth/controller/UserController.java
+++ src/main/java/com/sparta/springprepare/auth/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sparta.springprepare.auth.controller;
 
+import com.sparta.springprepare.auth.dto.LoginRequestDto;
 import com.sparta.springprepare.auth.dto.SignupRequestDto;
 import com.sparta.springprepare.auth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +34,16 @@ public class UserController {
 
         // 오른쪽의 주소로 URL 요청을 다시 진행한다
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
     }
 }
